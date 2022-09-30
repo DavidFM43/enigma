175860736bf984d3ecf99bb43a961044539c6786
David <dmorara@unal.edu.co>
2022-09-29 23:16:25 -0500

    Affine tests

---
+++ backend/tests/test_affine.py
@@ -0,0 +1,11 @@
+from cryptools.affine import encrypt, decrypt, attack
+
+def test_encrypt():
+    assert encrypt("hot", (7, 3)) == "AXG"
+
+def test_decrypt():
+    assert decrypt("AXG", (7, 3)) == "hot"
+
+def attack():
+    cadena = "FMXVEDKAPHFERBNDKRXRSREFMORUDSDKDVSHVUFEDKAPRKDLYEVLRHHRH"
+    pass
