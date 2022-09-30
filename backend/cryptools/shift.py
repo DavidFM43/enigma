71042a1dcaf664f0c36c62f18c8a5241aa1181ef
David <dmorara@unal.edu.co>
2022-09-29 22:56:46 -0500

    Shift tests

--- backend/cryptools/shift.py
+++ backend/cryptools/shift.py
@@ -2,13 +2,7 @@
 Shift cipher
 `key` must be an integer from Z_26.
 """
-from string import ascii_lowercase
-
-# char to int
-char2int = {x: idx for idx, x in enumerate(ascii_lowercase)}
-# int to char
-int2char = {idx: x for idx, x in enumerate(ascii_lowercase)}
-
+from cryptools.util import char2int, int2char
 
 def encrypt(plain_text: str, k: int) -> str:
     plain_text = plain_text.replace(" ", "").lower()
@@ -22,12 +16,5 @@ def decrypt(cipher_text: str, k: int) -> str:
 
 def attack(chipher_text: str):
     """Returns a dictionary of all 26 possible (decryption, key) pairs"""
-    arr_res = []
-    for k in range(26):
-        arr_res.append(decrypt(chipher_text, k))
-    return arr_res
+    return [decrypt(chipher_text, k) for k in range(26)]
 
-
-if __name__ == "__main__":
-    assert encrypt("attackatdown", 11) == "LEELNVLEOZHY"
-    assert decrypt("LEELNVLEOZHY", 11) == "attackatdown"
---
+++ backend/tests/test_shift.py
@@ -0,0 +1,10 @@
+from cryptools.shift import encrypt, decrypt, attack
+
+def test_encrypt():
+    assert encrypt("attackatdown", 11) == "LEELNVLEOZHY"
+
+def test_decrypt():
+    assert decrypt("LEELNVLEOZHY", 11) == "attackatdown"
+
+def test_attack():
+    assert attack("LEELNVLEOZHY") == ['leelnvleozhy', 'kddkmukdnygx', 'jccjltjcmxfw', 'ibbiksiblwev', 'haahjrhakvdu', 'gzzgiqgzjuct', 'fyyfhpfyitbs', 'exxegoexhsar', 'dwwdfndwgrzq', 'cvvcemcvfqyp', 'buubdlbuepxo', 'attackatdown', 'zsszbjzscnvm', 'yrryaiyrbmul', 'xqqxzhxqaltk', 'wppwygwpzksj', 'voovxfvoyjri', 'unnuweunxiqh', 'tmmtvdtmwhpg', 'sllsucslvgof', 'rkkrtbrkufne', 'qjjqsaqjtemd', 'piiprzpisdlc', 'ohhoqyohrckb', 'nggnpxngqbja', 'mffmowmfpaiz']
