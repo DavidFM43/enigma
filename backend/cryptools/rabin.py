ac7922e9642b18c43d80471d3e993ddeabea7853
David <dmorara@unal.edu.co>
2022-11-20 12:35:55 -0500

    Fix rabin
    - Solo funciona si los los bytes del texto son menores a n

--- backend/cryptools/rabin.py
+++ backend/cryptools/rabin.py
@@ -1,97 +1,86 @@
-
-"""
-1. Elegir primos grandes bajo la condicion
-2. Encriptar
-3. Desencriptar
-4. Elegir el correcto 
-
-"""
-
+import base64
 from Crypto.Util import number
 
-def prime_3mod4()->int:
-    """
-    return a prime n = p*q
-    """
+
+def prime_3mod4():
     while True:
-        p,q = number.getPrime(1024), number.getPrime(1024)
-        if p%4==3 and q%4 ==3: return p*q
+        p, q = number.getPrime(1024), number.getPrime(1024)
+        if p % 4 == 3 and q % 4 == 3:
+            return p, q
 
-def encrypt_rabin(plain_text:str, n:int)->int:
-    """
-    Function of encryption f(x)= x^2 mod n
-    """
-    plain_text = int(plain_text.replace(" ", "").lower(),36) #remove spaces and block code with base 32
-    print("texto codificado en bloque en base 36: ",plain_text)
-    return plain_text ** 2 % n
 
-def fast_pow(num, power, mod):
+def encrypt(plain_text: int, n: int) -> int:
+    return plain_text**2 % n
 
-    #result
-    res = 1 
-    # (b ^ k) * p = num ^ power
-    while power > 0:
-        while power % 2 == 0:
-            power /= 2
-            num = (num * num) % mod
-        power -= 1
-        res = (res * num) % mod
-    return res      
 
-def decrypt(cipher_text, p, q):
-    '''
-    Дешифровка одного элемента шифротекста
-    Возвращает List исходных элементов
-    '''
-    n = p * q #открытый ключ
+def decrypt(a, p, q):
+    n = p * q
+    r, s = 0, 0
 
-    mp = fast_pow(cipher_text, (p + 1) // 4, p)
-    mq = fast_pow(cipher_text, (q + 1) // 4, q)
+    r = sqrt_p_3_mod_4(a, p)
+    s = sqrt_p_3_mod_4(a, q)
 
-    #Расширенный алгоритм евклида
-    yp, yq, _gcd = extended_euclid(p, q) #третий возвращаемый параметр не используется
-    
-    
-    # di - корень из дискриминанта
-    d1 = (yp * p * mq + yq * q * mp) % n 
-    d2 = n - d1
-    d3 = (yp * p * mq - yq * q * mp) % n
-    d4 = n - d3
+    gcd, c, d = egcd(p, q)
+    x = (r * d * q + s * c * p) % n
+    y = (r * d * q - s * c * p) % n
 
-    d = (d1, d2, d3, d4)
- 
-    return d
+    return x, n - x, y, n - y
 
-def extended_euclid(a, b):
-    '''
-    Расширенный алгоритм Евклида
-    НОД(a, b) = x * a + y * b
-    Возвращает (x, y, gcd)
-    '''
 
-    #Индекс 1 - текущий элемет, 0 - предыдущий
-    x0, x1 = 1, 0
-    y0, y1 = 0, 1
-    while b:
-        # r2 = r0 - r1 * q
-        # r - остаток от деления
-        # q целая часть от a / b 
-        q = a // b 
+def sqrt_p_3_mod_4(a, p):
+    r = pow(a, (p + 1) // 4, p)
+    return r
 
-        a, b = b, a % b
 
-        # чтобы не создавать промежуточные переменные, присваивание кортежем
-        x0, x1 = x1, x0 - x1 * q
-        y0, y1 = y1, y0 - y1 * q
-    return (x0, y0, a)     
+def egcd(a, b):
+    if a == 0:
+        return b, 0, 1
+    else:
+        gcd, y, x = egcd(b % a, a)
+        return gcd, x - (b // a) * y, y
 
+
+def bytes2int(x):
+    return int.from_bytes(x, byteorder="big")
+
+
+def int2bytes(x):
+    return x.to_bytes((x.bit_length() + 7) // 8, byteorder="big")
+
+
+def bytes2str(x):
+    return base64.b64encode(x)
+
+
+def str2bytes(x):
+    return base64.b64decode(x)
 
 
 if __name__ == "__main__":
-    plain_text= "wxyz "
-    print("Bloque encriptado " ,encrypt_rabin(plain_text, 77))
-    l= decrypt(25, 11, 7)
-    print(l)
-    for i in l:
-        print(int(i,36))
-    
+    p, q = 864949991103707, 889118520858859
+    n = p * q
+
+    pt = "hola me llamo david"
+    pt = pt.replace(" ", "").lower()
+
+    # encryption
+
+    # decode to base64
+    enc = base64.b64decode(pt)
+    # cast bytes to int
+    enc_i = bytes2int(enc)
+    # enctypt int
+    ct_int = encrypt(enc_i, n)
+    # cast int to bytes
+    ct_bytes = int2bytes(ct_int)
+    # cast bytes to string with base64 encoding
+    ct = bytes2str(ct_bytes)
+    print(f"Texto encriptado: {ct}")
+
+    # decryption
+    ct_enc = str2bytes(ct)
+    ct_i = bytes2int(ct_enc)
+    pt_ops = decrypt(ct_i, p, q)
+    ct_ops = [bytes2str(int2bytes(op)) for op in pt_ops]
+    print("Opciones de texto claro")
+    print(ct_ops)
